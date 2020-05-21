2dc675a0bfa857151d6362426288d2eef447da7e
boypt <1033514+boypt@users.noreply.github.com>
2020-05-21 15:14:33 +0800

    init

---
+++ go.mod
@@ -0,0 +1,14 @@
+module serialdump
+
+go 1.14
+
+require (
+	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
+	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
+	github.com/lestrrat-go/strftime v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+	github.com/tebeka/strftime v0.1.4 // indirect
+	github.com/utahta/go-cronowriter v1.1.1
+	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299 // indirect
+)
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"path"
+	"time"
+
+	"github.com/tarm/serial"
+	"github.com/utahta/go-cronowriter"
+)
+
+var (
+	port       = flag.String("c", "", "serial port")
+	baud       = flag.Int("b", 115200, "baud rate")
+	logLoc     = flag.String("l", "", "log location")
+	logPrefix  = flag.String("p", "seraildump.log", "log prefix")
+	isSendTest = flag.Bool("t", false, "sent test")
+)
+
+func bgWrite(s *serial.Port) {
+	for {
+		time.Sleep(time.Second)
+		now := time.Now()
+		_, err := s.Write([]byte(now.String() + "\r\n"))
+		// log.Println(now.String())
+		if err != nil {
+			log.Fatal("writefatal", err)
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	logPath := path.Join(*logLoc, *logPrefix)
+	fmt.Println("logging to", logPath)
+	w := cronowriter.MustNew(logPath+".%Y%m%d",
+		cronowriter.WithSymlink(logPath),
+		cronowriter.WithInit(),
+	)
+	defer w.Close()
+
+	c := &serial.Config{Name: *port, Baud: *baud}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal("OpenPort", err)
+	}
+
+	if *isSendTest {
+		go bgWrite(s)
+	}
+
+	sc := bufio.NewScanner(s)
+	for sc.Scan() {
+		ts := time.Now().Format("2006-01-02 15:04:05")
+		w.Write([]byte(ts + " " + sc.Text() + "\n"))
+	}
+}
